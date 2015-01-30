b9fb136ab359d3e4bedba1f956a84ce1aa09ec7b
Artem Demo <artemdemo@gmail.com>
2015-01-30 22:00:32 +0200

    Home page basic desing

--- gen/me/artemdemo/moviedb/R.java
+++ gen/me/artemdemo/moviedb/R.java
@@ -28,9 +28,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f09000b;
+        public static final int btnSearch=0x7f09000a;
+        public static final int editText1=0x7f090004;
+        public static final int inputActor=0x7f090008;
+        public static final int inputYear=0x7f090002;
+        public static final int spinner1=0x7f090006;
         public static final int textView1=0x7f090000;
         public static final int textView2=0x7f090001;
+        public static final int textView3=0x7f090003;
+        public static final int textView4=0x7f090005;
+        public static final int textView5=0x7f090007;
+        public static final int toggleButton1=0x7f090009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -40,9 +49,15 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int adult_content_off=0x7f05000b;
+        public static final int adult_content_on=0x7f05000a;
         public static final int app_name=0x7f050000;
         public static final int fill_at_least_one_filed=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int search=0x7f050009;
+        public static final int title_actor=0x7f050007;
+        public static final int title_adultContent=0x7f050008;
+        public static final int title_genre=0x7f050006;
         public static final int title_title=0x7f050005;
         public static final int title_year=0x7f050004;
     }
