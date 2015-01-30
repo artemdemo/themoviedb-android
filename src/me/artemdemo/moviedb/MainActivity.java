27c71aa4ebe4dd80aaf6f877e51dfd6ca30c5e3e
Artem Demo <artemdemo@gmail.com>
2015-01-30 23:42:54 +0200

    Added search results activity

--- src/me/artemdemo/moviedb/MainActivity.java
+++ src/me/artemdemo/moviedb/MainActivity.java
@@ -1,9 +1,10 @@
 package me.artemdemo.moviedb;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -11,7 +12,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		final ProgressDialog progress = new ProgressDialog(this);
+		Button btnSearch = (Button) findViewById(R.id.btnSearch);
+		
+		btnSearch.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				/*progress.setTitle("Loading");
+				progress.setMessage("Wait while loading...");
+				progress.show();
+				// To dismiss the dialog
+				// progress.dismiss();*/
+			}
+		});
 	}
-
 
 }
---
+++ src/me/artemdemo/moviedb/SearchResultsActivity.java
@@ -0,0 +1,14 @@
+package me.artemdemo.moviedb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search_results);
+	}
+	
+}
